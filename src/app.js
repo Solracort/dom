04dada51068e832a73a6205831f7e8774fc0e3bf
Solracort <solracort@gmail.com>
2022-12-14 16:50:16 +0000

    FOR ANIDADOS

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".net", ".es"];
+  let name = "";
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < domain.length; l++) {
+          name = pronoun[i] + adj[j] + noun[k] + domain[l];
+          console.log(name);
+        }
+      }
+    }
+  }
   console.log("Hello Rigo from the console!");
 };
